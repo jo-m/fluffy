04355adda454737e4a1eb2bcaaa6c5b9a4067406
Jonathan Müller <jo-m@outlook.com>
2025-06-05 17:32:32 +0200

    opentelemetry-collector almost works

--- modules/default.nix
+++ modules/default.nix
@@ -2,6 +2,7 @@
   caddy = import ./caddy.nix;
   harden = import ./harden.nix;
   hetzner = import ./hetzner;
+  opentelemetry-collector = import ./opentelemetry-collector.nix;
   rootless-podman = import ./rootless-podman.nix;
   ssh = import ./ssh.nix;
   syncthing = import ./syncthing.nix;
---
+++ modules/opentelemetry-collector.nix
@@ -0,0 +1,102 @@
+{
+  tld,
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  serviceName = "opentelemetry-collector";
+  username = "opentelemetry-collector";
+  openobserve-url = "http://127.0.0.1:30004";
+  base64 = import ./base64.nix {inherit lib;};
+  # https://github.com/openobserve/agents/blob/bf46fe5a4d440e3621ba826d4034bdf0ca243ef7/linux/install.sh#L49
+  configFile = pkgs.writeText "opentelemetry-collector.yaml" ''
+    receivers:
+      journald:
+        directory: /var/log/journal
+      hostmetrics:
+        root_path: /
+        collection_interval: 30s
+        scrapers:
+          cpu:
+          disk:
+          filesystem:
+          load:
+          memory:
+          network:
+          paging:
+          processes:
+          # process: # a bug in the process scraper causes the collector to throw errors so disabling it for now
+    processors:
+      resourcedetection/system:
+        detectors: ["system"]
+        system:
+          hostname_sources: ["os"]
+      memory_limiter:
+        check_interval: 1s
+        limit_percentage: 75
+        spike_limit_percentage: 15
+      batch:
+        send_batch_size: 10000
+        timeout: 10s
+
+    extensions:
+      zpages: {}
+
+    exporters:
+      otlphttp/openobserve:
+        endpoint: ${openobserve-url}
+        headers:
+          Authorization: "Basic ''${env:BASIC_AUTH}"
+      otlphttp/openobserve_journald:
+        endpoint: ${openobserve-url}
+        headers:
+          Authorization: "Basic ''${env:BASIC_AUTH}"
+          stream-name: journald
+
+    service:
+      extensions: [zpages]
+      pipelines:
+        metrics:
+          receivers: [hostmetrics]
+          processors: [resourcedetection/system, memory_limiter, batch]
+          exporters: [otlphttp/openobserve]
+        logs:
+          receivers: [filelog/std]
+          processors: [resourcedetection/system, memory_limiter, batch]
+          exporters: [otlphttp/openobserve]
+        logs/journald:
+          receivers: [journald]
+          processors: [resourcedetection/system, memory_limiter, batch]
+          exporters: [otlphttp/openobserve_journald]
+  '';
+in {
+  users.users."${username}" = {
+    initialHashedPassword = "!";
+    isNormalUser = true;
+  };
+
+  sops.templates.openobserve-basicauth.content = let
+    concat = ''${config.sops.placeholder."openobserve/user"}:${config.sops.placeholder."openobserve/password"}'';
+  in ''
+    BASIC_AUTH=${base64.toBase64 concat}
+  '';
+  sops.templates.openobserve-basicauth.owner = username;
+
+  # https://github.com/openobserve/agents/blob/bf46fe5a4d440e3621ba826d4034bdf0ca243ef7/linux/install.sh#L115
+  systemd.services."${serviceName}" = {
+    enable = true;
+    wantedBy = ["multi-user.target"];
+    after = ["network.target" "network-online.target" "systemd-journald.service"];
+    serviceConfig = {
+      ExecStart = "${pkgs.opentelemetry-collector-contrib}/bin/otelcol-contrib --config ${configFile}";
+      User = username;
+      Group = "users";
+      Restart = "always";
+      RestartSec = "100ms";
+      RestartSteps = "10";
+      RestartMaxDelaySec = "60s";
+      EnvironmentFile = config.sops.templates.openobserve-basicauth.path;
+    };
+  };
+}
