f863cb51c1bada2913a4ffc727d0f7e1dcb43cac
Jonathan Müller <jo-m@outlook.com>
2025-06-08 11:36:43 +0200

    shuffle internal ports

--- containers/default.nix
+++ containers/default.nix
@@ -2,37 +2,37 @@
   echo = import ./echo.nix {
     service-name = "echo";
     domain = "echo";
-    internal-port = 30001;
+    internal-port = 30010;
   };
   ferrishare = import ./ferrishare.nix {
     service-name = "ferrishare";
     domain = "files";
-    internal-port = 30002;
+    internal-port = 30020;
   };
   openobserve = import ./openobserve.nix {
     service-name = "openobserve";
-    domain = "logs";
-    internal-port = 30003;
+    domain = "monitor";
+    internal-port = 30030;
   };
   qr = import ./qr.nix {
     service-name = "qr";
     domain = "qr";
-    internal-port = 30004;
+    internal-port = 30040;
   };
   readeck = import ./readeck.nix {
     service-name = "readeck";
     domain = "readeck";
-    internal-port = 30005;
+    internal-port = 30050;
   };
   hemmelig = import ./hemmelig.nix {
     service-name = "hemmelig";
     domain = "secrets";
-    internal-port = 30006;
+    internal-port = 30060;
   };
   traggo = import ./traggo.nix {
     service-name = "traggo";
     domain = "track";
-    internal-port = 30007;
+    internal-port = 30070;
   };
   webdav = import ./webdav.nix {
     service-name = "webdav";
