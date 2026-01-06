21e3153680134b29bc17103b390c6a8d874b4cf5
Jonathan M <jo-m@outlook.com>
2026-01-06 23:33:28 +0100

    pq safe algos (https://www.openssh.org/pq.html) and some other SSH settings

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -19,34 +19,33 @@
       KbdInteractiveAuthentication = true;
 
       KexAlgorithms = [
+        "sntrup761x25519-sha512@openssh.com"
+        "curve25519-sha256"
         "curve25519-sha256@libssh.org"
+        "diffie-hellman-group18-sha512"
         "diffie-hellman-group-exchange-sha256"
+        "diffie-hellman-group16-sha512"
       ];
       Ciphers = [
         "chacha20-poly1305@openssh.com"
         "aes256-gcm@openssh.com"
-        "aes128-gcm@openssh.com"
         "aes256-ctr"
         "aes192-ctr"
+        "aes128-gcm@openssh.com"
         "aes128-ctr"
       ];
       Macs = [
         "hmac-sha2-512-etm@openssh.com"
         "hmac-sha2-256-etm@openssh.com"
         "umac-128-etm@openssh.com"
-        "hmac-sha2-512"
-        "hmac-sha2-256"
-        "umac-128@openssh.com"
       ];
     };
     extraConfig = ''
       ClientAliveCountMax 0
       ClientAliveInterval 300
 
-      AllowTcpForwarding no
-      AllowAgentForwarding no
-      MaxAuthTries 3
-      MaxSessions 2
+      MaxAuthTries 10
+      MaxSessions 10
       TCPKeepAlive no
 
       PrintLastLog no
