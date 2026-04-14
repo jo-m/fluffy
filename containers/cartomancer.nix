a1021c5755ebc059b1197fd8e985e7e34be794f4
Jonathan M <jo-m@outlook.com>
2026-04-14 22:47:27 +0200

    enable cartomancer again, with memory limit and hardcoded version

--- containers/cartomancer.nix
+++ containers/cartomancer.nix
@@ -59,12 +59,13 @@ in {
           serviceConfig = containerLib.ServiceConfig;
           # https://docs.podman.io/en/latest/markdown/podman-systemd.unit.5.html
           containerConfig = {
-            image = "ghcr.io/jo-m/cartomancer:latest";
+            # https://github.com/jo-m/cartomancer/pkgs/container/cartomancer
+            image = "ghcr.io/jo-m/cartomancer:0.1.3";
             autoUpdate = "registry";
             name = cfg.serviceName;
 
             userns = "";
-            podmanArgs = ["--umask=0027"];
+            podmanArgs = ["--umask=0027" "--cpus=2"];
             publishPorts = ["127.0.0.1:${toString cfg.port}:8080"];
             mounts = ["type=bind,src=${data-base-dir}/${cfg.serviceName},dst=/data"];
             exec = "serve";
@@ -73,6 +74,7 @@ in {
               APP_REGISTRATION_ENABLED = "false";
               JOBS_MAX_PARALLEL = "1";
             };
+            memory = "2g";
             environmentFiles = [outerConfig.sops.templates.cartomancer-secret-env.path];
             labels = containerLib.podfatherLabels {
               name = "Cartomancer";
